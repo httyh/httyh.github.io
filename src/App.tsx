4706bfc033b4b5e49d2e407d353b9cc0bddb49e8
hutao <1131589588@qq.com>
2022-07-17 23:10:08 +0800

    feat: 基础配置eslint、prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      hello，钰涵
-    </div>
-  )
+  return <div className='App'>hello，钰涵</div>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import {
+  createStyleImportPlugin,
+  AndDesignVueResolve,
+  VantResolve,
+  ElementPlusResolve,
+  NutuiResolve,
+  AntdResolve,
+} from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    reactRefresh(),
+    createStyleImportPlugin({
+      resolves: [
+        AndDesignVueResolve(),
+        VantResolve(),
+        ElementPlusResolve(),
+        NutuiResolve(),
+        AntdResolve(),
+      ],
+      libs: [
+        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+        {
+          libraryName: 'ant-design',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design/es/${name}/style/index`
+          },
+        },
+      ],
+    })
+  ],
   build: {
     outDir: "docs"
-  }
+  },
 })
