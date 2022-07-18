45b144ae67050a2c4d0dec120ed2e29f0a3af39c
hutao <1131589588@qq.com>
2022-07-18 23:37:03 +0800

    chore: lock文件更新，处理运行报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import {
-  createStyleImportPlugin,
-  AndDesignVueResolve,
-  VantResolve,
-  ElementPlusResolve,
-  NutuiResolve,
-  AntdResolve,
-} from 'vite-plugin-style-import'
+// import {
+//   createStyleImportPlugin,
+//   AntdResolve,
+// } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    reactRefresh(),
-    createStyleImportPlugin({
-      resolves: [
-        AndDesignVueResolve(),
-        VantResolve(),
-        ElementPlusResolve(),
-        NutuiResolve(),
-        AntdResolve(),
-      ],
-      libs: [
-        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
-        {
-          libraryName: 'ant-design',
-          esModule: true,
-          resolveStyle: (name) => {
-            return `ant-design/es/${name}/style/index`
-          },
-        },
-      ],
-    })
+    // createStyleImportPlugin({
+    //   resolves: [
+    //     AntdResolve(),
+    //   ],
+    //   libs: [
+    //     // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+    //     {
+    //       libraryName: 'ant-design',
+    //       esModule: true,
+    //       resolveStyle: (name) => {
+    //         return `ant-design/es/${name}/style/index`
+    //       },
+    //     },
+    //   ],
+    // })
   ],
   build: {
     outDir: "docs"
