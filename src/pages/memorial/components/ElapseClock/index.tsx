efb643e5f4f8889241206c370658706f2f315fe7
hutao <1131589588@qq.com>
2022-08-05 22:28:25 +0800

    fix: 修复Safari上NaN问题

--- src/pages/memorial/components/ElapseClock/index.tsx
+++ src/pages/memorial/components/ElapseClock/index.tsx
@@ -12,8 +12,8 @@ import styles from './styles.module.less'
 
 dayjs.extend(durationPlugin)
 
-const startTime = '4-26-2022 21:30:00'
-const format = 'MM-DD-YYYY HH:mm:ss'
+const startTime = '4/26/2022 21:30:00'
+const format = 'MM/DD/YYYY HH:mm:ss'
 
 const timeDurtion = dayjs.duration(dayjs().diff(dayjs(startTime, format)))
 
